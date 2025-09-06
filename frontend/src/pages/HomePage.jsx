ad902d6f6e9f9025e2ffac91c16408f20e7880dd
Aayush Vyas <aayush25vyas@gmail.com>
2025-09-07 01:07:12 +0530

    Create RateLimiter UI Component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import MainLayout from "../layout/Layout";
+import RateLimitedComponent from "../components/RateLimitedComponent/RateLimitedComponent";
 
 function HomePage() {
+  const [isRateLimited, setIsRateLimited] = useState(true); 
+
     return <div className="min-h-screen"> 
        <MainLayout>
-
+         {isRateLimited && <RateLimitedComponent/>}
        </MainLayout>
     </div>
 }
