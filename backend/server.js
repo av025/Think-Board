2de1c4b08b4ba06175e33f046cd292d27af8cb2e
Aayush Vyas <aayush25vyas@gmail.com>
2025-09-09 00:51:36 +0530

    Project Completed and prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,33 @@ import { connectDB } from "./src/config/db.js";
 import notesRoutes from "./src/routes/notes.route.js";
 import rateLimiter from "./src/middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
-
 const SERVER_PORT = process.env.BACKEND_PORT;
-app.use(cors({
-  origin: process.env.FRONTEND_URL
-}))
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (request, response) => {
+    response.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
+  });
+}
 
 connectDB().then(() => {
   app.listen(SERVER_PORT, () => {
--- frontend/src/helpers/axiosInstances.js
+++ frontend/src/helpers/axiosInstances.js
@@ -1,7 +1,8 @@
 import axios from "axios"; 
 
+const BASE_URL = import.meta.env.VITE_MODE === "development" ? import.meta.env.VITE_BASE_URL : "/api"
 const API = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: BASE_URL
 }); 
 
 export default  API;
